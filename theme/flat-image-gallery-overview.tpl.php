8b7335895f4718caaa1a797b520db1669c9a9429
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2019-01-24 16:07:29 +0100

    adding pagination + enlarging thumbnails + adding filenames as labels

--- theme/flat-image-gallery-overview.tpl.php
+++ theme/flat-image-gallery-overview.tpl.php
@@ -5,16 +5,16 @@
  */
 ?>
 <div class="flat-image-gallery-container">
+    <h1><?php echo $islandora_object->label; ?> - images</h1>
     <div class="flat-image-gallery-thumbnail">
         <?php if (false !== $current) : ?>
             <?php echo $current['viewer']; ?>
-            <?php if (count($current['descriptions']) > 0) : ?>
-                <div class="flat-image-gallery-caption">
-                    <?php foreach ($current['descriptions'] as $description) : ?>
-                        <h4><?php echo $description; ?></h4>
-                    <?php endforeach; ?>
-                </div>
-            <?php endif;?>
+            <div class="flat-image-gallery-caption">
+                <?php echo $current['filename']; ?>
+                <?php foreach ($current['descriptions'] as $description) : ?>
+                    <h4><?php echo $description; ?></h4>
+                <?php endforeach; ?>
+            </div>
         <?php endif; ?>
     </div>
     <div class="row">
@@ -29,20 +29,24 @@
         <div class="col-lg-12">
             <div class="row">
                 <?php foreach ($images as $image) : ?>
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <a href="<?php echo $image['url']; ?>" class="flat-image-gallery-thumbnail <?php echo $current['pid'] === $image['pid'] ? 'active' : ''; ?>">
-                        <img style="height: 100px;" src="<?php echo $image['thumbnail']; ?>" />
-                        <?php if (count($image['descriptions']) > 0) : ?>
-                            <div class="flat-image-gallery-caption">
-                                <?php foreach ($image['descriptions'] as $description) : ?>
-                                    <small><?php echo $description; ?></small><br />
-                                <?php endforeach; ?>
-                            </div>
-                        <?php endif; ?>
+                        <img src="<?php echo $image['thumbnail']; ?>" />
+                        <div class="flat-image-gallery-caption">
+                            <small><?php echo $image['filename']; ?></small>
+                        </div>
                     </a>
                 </div>
                 <?php endforeach; ?>
             </div>
+        </div>
+    </div>
+    <hr />
+    <div class="row">
+        <div class="col-lg-offset-5">
+            <p>
+                <?php echo implode(' | ', $paginationLinks); ?>
+            </p>
         </div>
     </div>
 </div>
